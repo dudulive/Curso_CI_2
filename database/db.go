be9d3402558c9139fae181eebccd6800d6e9b726
dudulive <edurodriguesbraz@hotmail.com>
2025-10-23 17:20:09 -0300

    new db

--- database/db.go
+++ database/db.go
@@ -14,11 +14,11 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-    host := os.Getenv("DBHOST")
-    user := os.Getenv("DBUSER")
-    password := os.Getenv("DBPASSWORD")
+    host := os.Getenv("HOST")
+    user := os.Getenv("USER")
+    password := os.Getenv("PASSWORD")
     dbname := os.Getenv("DBNAME")
-    port := os.Getenv("DBPORT")
+    port := os.Getenv("PORT")
     stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
